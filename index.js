2a689db0ee00e53021e0803cb9ca90fd08022678
Austin Harlow <harlowaustin2@gmail.com>
2019-04-27 23:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 var recipies = new Object ({})
 
 var obj = { foo: 'bar'}
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, obj, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
