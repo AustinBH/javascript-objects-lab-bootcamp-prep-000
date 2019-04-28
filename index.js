c8396292e8e97c748e1e570e28f9f5698cd970e2
Austin Harlow <harlowaustin2@gmail.com>
2019-04-28 00:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+  var obj = { prop: 1}
   var newObj = Object.assign({}, obj);
-  delete newObj.key;
+  delete newObj.prop;
   return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object
+  
 }
