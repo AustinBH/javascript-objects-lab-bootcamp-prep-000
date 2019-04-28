c72e810b654ec58d6123547f35cdf49f6e12d373
Austin Harlow <harlowaustin2@gmail.com>
2019-04-28 00:06:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var recipes = {};
+var recipes = new Object({});
 
+var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]:value})
 }
